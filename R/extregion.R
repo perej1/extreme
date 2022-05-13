eaa79c6db93b6e3640218b8bdd89070c49be4645
perej1 <jaakko.pere@aalto.fi>
2022-05-13 10:21:13 +0300

    Document extregion.

--- R/extregion.R
+++ R/extregion.R
@@ -1,3 +1,25 @@
+#' Estimate Extreme Quantile Region
+#'
+#' Estimates (1-p)-quantile region for a very small p.
+#'
+#' Estimator is consistent only for heavy-tailed elliptical distributions.
+#' Extreme value index is estimated with separating Hill estimator.
+#'
+#' @param x A matrix representing sample from elliptical distribution.
+#'   Rows are observations and columns dimensions.
+#' @param p Probability corresponding to \code{(1-p)}-quantile. Values in
+#'   interval \eqn{(0, 1)} are accepted.
+#' @param k Threshold parameter for the estimator. Threshold parameter has to
+#'   be in set \eqn{{1,2,\ldots,n}} where \eqn{n} is equal to \code{nrow(x)}.
+#' @param method How location and scatter should be estimated?
+#' @param alpha Controls size of the subsets over which the determinant is
+#'   minimized when \code{method = "mcd"}.
+#'
+#' @return A ellipsoidq object representing the estimate.
+#' @export
+#'
+#' @examples
+#' TODO
 extregion <- function(x, p, k, method = "sample", alpha = NULL) {
   if (method %!in% c("sample", "mcd")) {
     abort("Invalid 'method'")
