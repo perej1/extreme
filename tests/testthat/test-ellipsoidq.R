7b0a6df750cc21105f6155ec56d4ced41bfe268c
perej1 <jaakko.pere@aalto.fi>
2022-05-24 13:51:07 +0300

    Remove numerical integration

--- tests/testthat/test-ellipsoidq.R
+++ tests/testthat/test-ellipsoidq.R
@@ -1,10 +1,3 @@
-test_that("mcint routine works for constant functions", {
-  sigma <- matrix(c(11, 10.5, 10.5, 11.25), nrow = 2, byrow = TRUE)
-  mu <- c(-1, 1)
-  df <- 10
-  d <- length(mu)
-  p <- 0.5
-  q_radius <- sqrt(d * qf(1 - p, d, df))
-  x <- ellipsoidq(mu, sigma, q_radius)
-  integrate(function(x) mvtdens(x, mu, sigma, df), x, x, n = 10000)
+test_that("dummy", {
+  expect_equal(2*2, 4)
 })
