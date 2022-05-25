54da0c60cdae25256b255be62292849721dd188a
perej1 <jaakko.pere@aalto.fi>
2022-05-25 23:30:41 +0300

    Move univariate and multivariate extreme quantile estimators in same file

--- tests/testthat/test-extregion.R
+++ tests/testthat/test-extregion.R
@@ -1,3 +1,20 @@
+test_that("extquantile gives correct outputs", {
+  x <- c(10, 7, 5, 4, 1, 6, 7, 8, 4, 7)
+  y <- sort(x, decreasing = FALSE)
+  n <- length(x)
+  k <- c(1, 3, 5, length(x) - 1)
+  g <- c(
+    log(5 / 4), 1 / 3 * log(80 / 49), 1 / 5 * log(1715 / 486),
+    1 / 9 * log(13171200)
+  )
+  p <- 10e-3
+  q <- y[n - k] * (k / (n * p)) ^ g
+  expect_equal(extquantile(x, p, k[1]), q[1])
+  expect_equal(extquantile(x, p, k[2]), q[2])
+  expect_equal(extquantile(x, p, k[3]), q[3])
+  expect_equal(extquantile(x, p, k[4]), q[4])
+})
+
 n <- 1000
 mu <- c(1, -1)
 sigma <- matrix(c(11, 10.5, 10.5, 11.25), nrow = 2, byrow = TRUE)
@@ -23,4 +40,12 @@ test_that("error is thrown if method is mcd but alpha is invalid", {
     "'method == mcd' but value for alpha is not given"
   )
   expect_error(extregion(x, 10e-3, 10, method = "mcd", alpha = "a"))
+})
+
+test_that("error is thrown when k is invalid", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("error is thrown when p is invalid", {
+  expect_equal(2 * 2, 4)
 })
